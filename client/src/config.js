298471e6194d7952612182e70b7bcef21b663688
rkredblox <radhakrishnan.g@redblox.io>
2025-05-20 16:31:34 +0530

    change url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://homesnaks.onrender.com';
+
 
 export default API_URL; 
